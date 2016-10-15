c7b334e9dcb4711f075c90c4a7743b4c68b79ea0
Nabeel Khan <rockystorm@gmail.com>
2016-10-14 19:00:04 -0700

    Added Navigation Controller

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -32,6 +32,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.textLabel?.text = emojis[indexPath.row]
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "moveSegue", sender: "😇")
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
